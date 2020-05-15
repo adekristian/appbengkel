9b4c0e0d9bc95bae6db32105fac23cff1f49b410
adekristian <krsitianade098@gmail.com>
2020-05-16 00:03:24 +0700

    berhasil nambah data

--- controller.js
+++ controller.js
@@ -3,6 +3,40 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req,res){
+exports.index = function(req, res){
     response.ok("Aplikasi Res Apiku berjalan", res)
+};
+
+//menapilkan data mahasiswa
+
+exports.tampilsemuamahasiswa = function(req,res){
+connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+    if (error){
+        connection.log(error);
+
+    }
+    else{
+        response.ok(rows, res)
+    }
+});
+};
+
+
+//menambahkan data mahasiswa
+
+exports.tambahMahasiswa = function(req,res){
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)',
+    [nim, nama, jurusan],
+    function (error, rows, fileds){
+        if (error){
+        Console.log(error);
+    }
+    else {
+        response.ok("Berhasil Menambahkan data",res)
+    }
+});
 };
--- routes.js
+++ routes.js
@@ -5,4 +5,8 @@ module.exports = function(app) {
 
     app.route('/')
     .get(jsonku.index);
+    app.route('/tampil')
+    .get(jsonku.tampilsemuamahasiswa);
+    app.route('/tambah')
+    .post(jsonku.tambahMahasiswa);
 }
